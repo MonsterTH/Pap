74de3752e05dc266336bbe4da2966fac5516f956
MonsterTH <qingyunyang8888@gmail.com>
2026-01-13 16:55:50 +0000

    coisas para o gui

--- home.php
+++ home.php
@@ -4,8 +4,19 @@
       <meta charset="UTF-8">
       <link rel="stylesheet" href="scripts/moderno.css">    
       <link href="https://fonts.googleapis.com/css2?family=EB+Garamond:ital,wght@0,400..800;1,400..800&family=Oxygen:wght@300;400;700&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+      <?php
+            session_start();
+            if (!isset($_SESSION['Username'])) {
+            header('Location: ../utilizador/login.html');
+            exit();
+            }
+      ?>
 </head>
 <body>
+
+      <?php
+            $username = htmlspecialchars($_SESSION['Username']);
+      ?>
 
       <div class="fundo">
             <div class="logo"> 
@@ -15,18 +26,18 @@
       
       <nav class="bar">
             <ul>
-                  <li><a href="register/register.html"><b>Início</b></a></li>
-                  <li><a href="register/register.html"><b>Personagens</b></a></li>
-                  <li><a href="register/register.html"><b>Notícias</b></a></li>
-                  <li><a href="register/register.html"><b>Votos</b></a></li>
-                  <li><a href="register/register.html"><b>Sobre</b></a></li>
-                  <li><a href="register/register.html"><b>Login/Registar</b></a></li>
+                  <li><a href="#"><b>Início</b></a></li>
+                  <li><a href="#"><b>Personagens</b></a></li>
+                  <li><a href="#"><b>Notícias</b></a></li>
+                  <li><a href="#"><b>Votos</b></a></li>
+                  <li><a href="#"><b>Sobre</b></a></li>
+                  <li><a href="user/user.php"><b>Bem-vindo, <?php echo $username; ?></b></a></li>
             </ul>
       </nav>
 
       <div class="noti">
             <div>
-                  <img class="img_noti" src="imgs/ns3.jpg" >
+                  <img class="img_noti" src="imgs/ns4.jpg" >
             </div>
             <div class="texto_noti">
                   <center><h1>Notícias</h1></center>
