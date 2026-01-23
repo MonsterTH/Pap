7e093922240aa53c0a761819f1ed7ff03389ad5b
MonsterTH <qingyunyang8888@gmail.com>
2026-01-23 15:35:58 +0000

    AJAX_User_Update

--- scripts/functions.js
+++ scripts/functions.js
@@ -101,3 +101,44 @@ function del_user(event)
 
     xmlhttp.send(formdata);
 }
+
+// Atualização do user
+function erro_user(event) 
+{
+    event.preventDefault(); // Previne o envio padrão do formulário
+
+    var erro = document.getElementById("erro");
+    var pass = document.getElementById("pass_new").value.trim();
+    var pass_rep = document.getElementById("pass_rep").value.trim();
+
+    if (pass !== pass_rep) 
+    {
+        erro.textContent = "As senhas novas não coincidem";
+        erro.style.color = "red";
+        return;
+    } 
+
+    const form = document.getElementById("user_form");
+    const formdata = new FormData(form);
+
+    const xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("POST", "user_update.php", true);
+
+    xmlhttp.onload = function () 
+    {
+        if (xmlhttp.status === 200) 
+        {
+            if (xmlhttp.responseText.trim() === "ok") 
+            {
+                window.location.href = "../home.php";
+            } 
+            else 
+            {
+                erro.textContent = xmlhttp.responseText;
+                erro.style.color = "red";
+            }
+        }
+    };
+
+    xmlhttp.send(formdata);
+}
