192fac8815485ac05d214a6793f48106c382205d
Madeira-Guilherme <guirbobers@gmail.com>
2026-03-30 17:36:15 +0100

    Logos no rodape que estavam a deixar-me maluco

--- site_laravel/identityfraud/resources/views/partials/footer.blade.php
+++ site_laravel/identityfraud/resources/views/partials/footer.blade.php
@@ -22,13 +22,13 @@
                             <div class="flex flex-col gap-4">
                                     <div class="flex items-center gap-3 text-brand-muted text-sm">
                                         <div class="w-8 h-8 flex items-center justify-center rounded-lg bg-brand-accent/10">
-                                                <img src="imgs/telephone.png" alt="Telefone" class="w-4 h-4 brightness-200">
+                                                <svg xmlns="http://www.w3.org/2000/svg" fill="#FFFFFF" viewBox="0 0 48 48" id="Phone--Streamline-Plump" height="48" width="48"><g id="phone--android-phone-mobile-device-smartphone-iphone"><path id="Union" stroke="#000000" stroke-linejoin="round" d="M45 13c0 -5.523 -4.477 -10 -10 -10h-0.569a3 3 0 0 0 -2.785 1.886L29.18 11.05a4 4 0 0 0 0.885 4.314l3.077 3.077c0.493 0.493 0.7 1.21 0.498 1.878a20.045 20.045 0 0 1 -13.32 13.32c-0.668 0.203 -1.385 -0.005 -1.878 -0.498l-3.077 -3.077a4 4 0 0 0 -4.314 -0.885l-6.165 2.466A3 3 0 0 0 3 34.43V35c0 5.523 4.477 10 10 10h0.5c17.11 0 31.034 -13.642 31.489 -30.642 0 -0.005 0.002 -0.01 0.005 -0.013a0.018 0.018 0 0 0 0.006 -0.013V13Z" stroke-width="3"></path></g></svg>
                                         </div>
                                         +351-933441581
                                     </div>
                                     <div class="flex items-center gap-3 text-brand-muted text-sm">
                                         <div class="w-8 h-8 flex items-center justify-center rounded-lg bg-brand-accent/10">
-                                                <img src="imgs/envelope.png" alt="Email" class="w-4 h-4 brightness-200">
+                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 35 35" id="Mail-Send--Streamline-Plump" height="35" width="35"><g id="mail-send--send-email-envelope-dash"><path id="Rectangle 1098" stroke="#FFFFFF" stroke-linejoin="round" d="M9.167660833333333 23.587655833333333c0.10799958333333333 1.1606537499999998 1.0130087499999998 2.0328529166666667 2.175029583333333 2.1210297916666665C12.999595416666667 25.833773749999995 15.767255624999999 25.97458333333333 19.82270833333333 25.97458333333333s6.823112916666665 -0.14080958333333332 8.480017916666666 -0.26658125c1.1620208333333333 -0.08749333333333333 2.06703 -0.9596924999999998 2.175029583333333 -2.12034625 0.13670833333333332 -1.4702981249999998 0.28161916666666664 -3.8203143749999997 0.28161916666666664 -7.182655833333332 0 -3.3630249999999995 -0.14491083333333332 -5.712357708333332 -0.28161916666666664 -7.182655833333332 -0.10799958333333333 -1.1606537499999998 -1.0130087499999998 -2.0328529166666667 -2.175029583333333 -2.1210297916666665C26.645821249999997 6.976226249999999 23.87884458333333 6.835416666666666 19.82270833333333 6.835416666666666s-6.823112916666665 0.14080958333333332 -8.479334374999999 0.26658125c-1.1620208333333333 0.08749333333333333 -2.0677135416666665 0.9596924999999998 -2.1757131249999997 2.12034625C9.0309525 10.692642291666667 8.886041666666666 13.041974999999997 8.886041666666666 16.404999999999998c0 3.3630249999999995 0.14491083333333332 5.712357708333332 0.28161916666666664 7.182655833333332Z" stroke-width="2.19"></path><path id="Intersect" stroke="#FFFFFF" stroke-linecap="round" stroke-linejoin="round" d="M8.886041666666666 15.037916666666666s7.940019999999999 4.101249999999999 10.936666666666666 4.101249999999999c2.995963125 0 10.936666666666666 -4.101249999999999 10.936666666666666 -4.101249999999999" stroke-width="2.19"></path></svg>
                                         </div>
                                         Support@IdentityFraud.com
                                     </div>
@@ -41,16 +41,13 @@
                             <p class="text-brand-muted text-sm leading-relaxed mb-5">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia veniam expedita quod odit quaerat maiores.</p>
                             <div class="flex items-center gap-3">
                                 <a href="#" class="social-icon w-10 h-10 flex items-center justify-center rounded-xl bg-white/5 border border-white/5">
-                                    <img src="imgs/facebook.png" alt="Facebook" class="w-5 h-5 brightness-200">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="-1.04 -1.04 25 25" fill="none" stroke="#FFFFFF" stroke-linecap="round" stroke-linejoin="round" id="Facebook--Streamline-Lucide" height="25" width="25"><path d="M17.19 1.9100000000000001h-2.865a4.775 4.775 0 0 0 -4.775 4.775v2.865H6.6850000000000005v3.8200000000000003h2.865v7.640000000000001h3.8200000000000003v-7.640000000000001h2.865l0.9550000000000001 -3.8200000000000003h-3.8200000000000003V6.6850000000000005a0.9550000000000001 0.9550000000000001 0 0 1 0.9550000000000001 -0.9550000000000001h2.865z" stroke-width="2.08"></path></svg>
                                 </a>
                                 <a href="#" class="social-icon w-10 h-10 flex items-center justify-center rounded-xl bg-white/5 border border-white/5">
-                                    <img src="imgs/youtube.png" alt="YouTube" class="w-5 h-5 brightness-200">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="#FFFFFF" class="bi bi-twitter-x" viewBox="0 0 16 16" id="Twitter-X--Streamline-Bootstrap" height="16" width="16"><path d="M12.6 0.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867 -5.07 -4.425 5.07H0.316l5.733 -6.57L0 0.75h5.063l3.495 4.633L12.601 0.75Zm-0.86 13.028h1.36L4.323 2.145H2.865z" stroke-width="1"></path></svg>
                                 </a>
                                 <a href="#" class="social-icon w-10 h-10 flex items-center justify-center rounded-xl bg-white/5 border border-white/5">
-                                    <img src="imgs/insta.png" alt="Instagram" class="w-5 h-5 brightness-200">
-                                </a>
-                                <a href="#" class="social-icon w-10 h-10 flex items-center justify-center rounded-xl bg-white/5 border border-white/5">
-                                    <img src="imgs/tiktok.png" alt="TikTok" class="w-5 h-5 brightness-200">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="-1.04 -1.04 25 25" fill="none" stroke="#FFFFFF" stroke-linecap="round" stroke-linejoin="round" id="Instagram--Streamline-Lucide" height="25" width="25"><path d="M6.6850000000000005 1.9100000000000001h9.55s4.775 0 4.775 4.775v9.55s0 4.775 -4.775 4.775H6.6850000000000005s-4.775 0 -4.775 -4.775V6.6850000000000005s0 -4.775 4.775 -4.775" stroke-width="2.08"></path><path d="M15.280000000000001 10.85835A3.8200000000000003 3.8200000000000003 0 1 1 12.061650000000002 7.640000000000001 3.8200000000000003 3.8200000000000003 0 0 1 15.280000000000001 10.85835z" stroke-width="2.08"></path><path d="m16.712500000000002 6.2075000000000005 0.009550000000000001 0" stroke-width="2.08"></path></svg>
                                 </a>
                         </div>
                     </div>
