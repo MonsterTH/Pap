7d5f5639ed04930cbf429a2b779c346b1e7b3afe
Madeira-Guilherme <guirbobers@gmail.com>
2026-04-07 12:16:05 +0100

    login_resolve

--- site_laravel/identityfraud/resources/views/layouts.blade.php
+++ site_laravel/identityfraud/resources/views/layouts.blade.php
@@ -189,7 +189,6 @@
         </main>
 
         @include('partials.footer')
-        <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
         @livewireScripts
     </body>
 </html>
