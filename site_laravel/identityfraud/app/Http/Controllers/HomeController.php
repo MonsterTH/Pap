b34ebbe7bf2ea8a0123a148ff2b7c4139c29290d
MonsterTH <qingyunyang8888@gmail.com>
2026-06-17 10:49:19 +0100

    coisas

--- site_laravel/identityfraud/app/Http/Controllers/HomeController.php
+++ site_laravel/identityfraud/app/Http/Controllers/HomeController.php
@@ -22,33 +22,33 @@ class HomeController extends Controller
         return view('index', compact('players', 'trending', 'novidades'));
     }
 
-public function watch($camera = 1)
-{
-    $cameras = [
-        1 => [
-            'name' => 'Camera 1',
-            'location' => 'Cozinha',
-            'video' => asset('CameraVideos/Cozinha.mp4'),
-        ],
-        2 => [
-            'name' => 'Camera 2',
-            'location' => 'Sala de Estar',
-            'video' => asset('CameraVideos/SalaDeEstar.mp4'),
-        ],
-        3 => [
-            'name' => 'Camera 3',
-            'location' => 'Exterior 1',
-            'video' => asset('CameraVideos/Exterior1.mp4'),
-        ],
-        4 => [
-            'name' => 'Camera 4',
-            'location' => 'Exterior 2',
-            'video' => asset('CameraVideos/Exterior2.mp4'),
-        ],
-    ];
+    public function watch($camera = 1)
+    {
+        $cameras = [
+            1 => [
+                'name' => 'Camera 1',
+                'location' => 'Cozinha',
+                'video' => asset('CameraVideos/Cozinha.mp4'),
+            ],
+            2 => [
+                'name' => 'Camera 2',
+                'location' => 'Sala de Estar',
+                'video' => asset('CameraVideos/SalaDeEstar.mp4'),
+            ],
+            3 => [
+                'name' => 'Camera 3',
+                'location' => 'Exterior 1',
+                'video' => asset('CameraVideos/Exterior1.mp4'),
+            ],
+            4 => [
+                'name' => 'Camera 4',
+                'location' => 'Exterior 2',
+                'video' => asset('CameraVideos/Exterior2.mp4'),
+            ],
+        ];
 
-    $selectedCamera = $cameras[$camera] ?? $cameras[1];
+        $selectedCamera = $cameras[$camera] ?? $cameras[1];
 
-    return view('watch', compact('cameras', 'selectedCamera'));
-}
+        return view('watch', compact('cameras', 'selectedCamera'));
+    }
 }
